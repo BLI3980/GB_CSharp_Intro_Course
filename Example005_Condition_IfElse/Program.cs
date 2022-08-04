27ef7aa0c97c3892613d3748aa8f40884176c609
Boris Li <liba8936@protonmail.com>
2022-08-05 00:05:02 +0400

    Add Example 005 Condition IfElse

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter you name: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "dude")
+{
+    Console.WriteLine("Hey, DUDE!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
