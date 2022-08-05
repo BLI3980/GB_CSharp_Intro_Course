00294c0ff04e9e9d7181d6c951b0d9cdfc1a591d
Boris Li <liba8936@protonmail.com>
2022-08-05 22:41:36 +0400

    Added Tasks 1-3

---
+++ Task002_CheckIfSquare/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b*b == a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
---
+++ Task003_Weekday/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n == 1) 
+{
+    Console.Write("Понедельник");
+}
+if(n == 2) 
+{
+    Console.Write("Вторник");
+}
+if(n == 3) 
+{
+    Console.Write("Среда");
+}
+if(n == 4) 
+{
+    Console.Write("Четверг");
+}
+if(n == 5) 
+{
+    Console.Write("Пятница");
+}
+if(n == 6) 
+{
+    Console.Write("Суббота");
+}
+if(n == 7) 
+{
+    Console.Write("Воскресение");
+}
+if(n > 7 || n < 1)
+{
+    Console.Write("Неверное значение");
+}
+/**/
