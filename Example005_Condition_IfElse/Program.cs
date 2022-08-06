f1bda6392b14be4d30cb8fce5fd934e9bb3ab1bc
Boris Li <liba8936@protonmail.com>
2022-08-06 22:01:21 +0400

    Minor changes

--- Example005_Condition_IfElse/Program.cs
+++ Example005_Condition_IfElse/Program.cs
@@ -1,11 +1,11 @@
-﻿Console.Write("Enter you name: ");
+﻿Console.Write("Enter your name: ");
 string username = Console.ReadLine();
 if(username.ToLower() == "dude")
 {
-    Console.WriteLine("Hey, DUDE!");
+    Console.WriteLine("Hey, DUDE! What's up?");
 }
 else
 {
     Console.Write("Hello, ");
-    Console.WriteLine(username);
+    Console.WriteLine(username); 
 }
--- Sem01_Homework08_EvenRange/Program.cs
+++ Sem01_Homework08_EvenRange/Program.cs
@@ -1,9 +1,11 @@
-﻿Console.Write("Enter an integer number: ");
+﻿//An integer number is entered. The output shows the range of numbers from 1 till the number entered.
+
+Console.Write("Enter an integer number: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 if (N < 1)
 {
-    Console.WriteLine("No even numbers for in this case");
+    Console.WriteLine($"No positive even numbers from 1 to {N}");
 }
 else
 {
