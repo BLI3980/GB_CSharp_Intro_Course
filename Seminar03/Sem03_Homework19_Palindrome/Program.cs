b1eb5a495d2785bdc6b78868e678e3608fa70515
Boris Li <liba8936@protonmail.com>
2022-08-13 02:54:23 +0400

    Completed Sem03 homework 19 and 23

---
+++ Seminar03/Sem03_Homework19_Palindrome/Program.cs
@@ -0,0 +1,20 @@
+﻿//Task: A five digit number is entered. The output is to check if the number is palindrome or not.
+
+Console.WriteLine("Enter an integer five-digit number: ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine())); 
+
+if ((n < 100000) && (n >= 10000))
+{
+    if ((n / 10000 == n % 10) && ((n / 1000) % 10 == (n % 100) /10))
+    {
+        Console.Write("The number is a palindrome.");
+    }
+    else
+    {
+        Console.Write("The number is NOT a palindrome.");
+    }
+}
+else
+{
+    Console.Write("The number you've entered in NOT a five-digit number. Try again.");
+}
---
+++ Seminar03/Sem03_Homework23_CubeRange/Program.cs
@@ -0,0 +1,92 @@
+﻿//Task: A number N is entered through the console. The output is the range of numbers from 1 to N powered to 3 and comma separated.
+
+ //OPTION 1. WITH ONE "FOR" LOOP =====================
+Console.WriteLine("Enter an integer number: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+int stopCycle = Math.Abs(n);
+
+if (n <= 0) Console.Write("1, 0, ");
+
+for ( int count = 1; count <= stopCycle; count++)
+{
+    if (n <= 0)
+    {
+        Console.Write(-Math.Pow(count,3));
+    }
+    else
+    {
+        Console.Write(Math.Pow(count,3));
+    }
+    if (count < stopCycle)
+    {
+        Console.Write(", ");
+    }
+}
+ //=============================================
+
+/* //OPTION 2. WITH TWO "FOR" LOOPS ==============
+Console.WriteLine("Enter an integer number: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+int count = 1;
+
+if (n <= 0) 
+{
+    for (count = 1; count >= n; count--)
+    {
+        Console.Write(Math.Pow(count,3));
+        if (count > n)
+        {
+        Console.Write(", ");
+        }
+    }
+    
+}
+else
+{
+    for (count = 1; count <= n; count++)
+    {
+        Console.Write(Math.Pow(count,3));
+        if (count < n)
+        {
+        Console.Write(", ");
+        }
+    }
+    
+}
+*/ //=============================================
+
+/* //OPTION 3. WITH TWO "WHILE" LOOPS ============
+Console.WriteLine("Enter an integer number: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+//int stopCycle = Math.Abs(n);
+int count = 1;
+
+if (n <= 0) 
+{
+    while (count >= n)
+    {
+        Console.Write(Math.Pow(count,3));
+        if (count > n)
+        {
+        Console.Write(", ");
+        }
+        count--;
+    }
+    
+}
+else
+{
+    while (count <= n)
+    {
+        Console.Write(Math.Pow(count,3));
+        if (count < n)
+        {
+        Console.Write(", ");
+        }
+        count++;
+    }
+}   
+*/ //=============================================
