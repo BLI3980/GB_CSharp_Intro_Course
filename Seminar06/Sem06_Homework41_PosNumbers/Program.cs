7eda2edc6c7072f09d8d58242efb02a454ca3113
Boris Li <liba8936@protonmail.com>
2022-08-24 16:15:50 +0400

    Completed Seminar 06 homework

---
+++ Seminar06/Sem06_Homework41_PosNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿//Task: Several numbers are entered through the console. Find out how many of them are larger than zero.
+
+Console.WriteLine("Enter how many numbers you want to see listed: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void FillPrintArray(int[] arr) //Randomly fill and print an array of m elements
+{
+    Console.WriteLine();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 9);
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+int PosElements(int[] arr) //Finding out how many positive elements in the array
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] array = new int[m];
+
+FillPrintArray(array);
+int count = PosElements(array);
+Console.WriteLine("The amount of elements larger than zero is: " + count);
+Console.WriteLine();
+
+
+
---
+++ Seminar06/Sem06_Homework43_LinesCross/Program.cs
@@ -0,0 +1,24 @@
+﻿//Task: Write a program which find the cross point of two lines derived from equations y = k1*x + b1 and y = k2*x + b2. k1, b1, k2, b2 are given by user entry.
+
+Console.WriteLine("Given equations are y = k1*x + b1 and y = k2*x + b2.");
+Console.WriteLine("Enter k1, b1, k2 and b2 coefficients: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+if (k1 == k2)
+{
+    Console.WriteLine("The two lines are parallel to each other, therefore have no cross points.");
+}
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("The two lines overlay each other, therefore have infinite amount of cross points.");
+}
+else
+{
+    Console.WriteLine($"The cross point of given two lines has these coordinates: x({x}), y({y})");
+}
