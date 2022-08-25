c6d82317bd2800dc05b7417f91891f6d11c904df
Boris Li <liba8936@protonmail.com>
2022-08-25 23:47:57 +0400

    minor changes

---
+++ Lecture05/Example/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Linq;
+
+var data = new int[] { 1, 2, 3, 4 }
+        .Where(e => e > 0)
+        .Select(e => new { q = e, w = e + 1 });
+Console.WriteLine(data.GetType()); // In this case we can't use anything other than "var" - it won't work
--- Seminar06/Sem06_Task02_DecToBin/Program.cs
+++ Seminar06/Sem06_Task02_DecToBin/Program.cs
@@ -3,16 +3,40 @@
 //Then all remainders lined up in reverse order are the number in binary system
 //23(dec) -> 10111(bin) => 23/2 - r[0] = 1, q[0] = 11; 11/2 - r[1] = 1, q[1] = 5; 5/2 - r[2] = 1, q[2] = 2; 2/2 - r[3] = 0, q[3] = 1; 1/2 - r[4] = 1, q[4] = 0.
 
-Console.WriteLine("Enter a decimal number:");
-int a = Convert.ToInt32(Console.ReadLine());
+// //OPTION 1 ================================================
+// Console.WriteLine("Enter a decimal number:");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-string bin = String.Empty; // Declare an empty string
+// string bin = String.Empty; // Declare an empty string
 
-while (a >= 1)
+// while (a >= 1)
+// {
+//     bin = a % 2 + bin; // Add a digit-character to the left of existing string. Then output "bin"
+//     Console.Write(a % 2); // Just to show which digit-character outputs at each iteration (reverse order of numbers)
+//     a = a / 2;
+// }
+// Console.WriteLine();
+// Console.Write($"Your number corresponds to {bin} in binary system.");
+
+//OPTION 2 ================================================
+string DecimalToBinary(int input) //Method for converting decimal number to a string representing the number in binary system
 {
-    bin = a % 2 + bin; // Add a digit-character to the left of existing string. Then output "bin"
-    Console.Write(a % 2); // Just to show which digit-character outputs at each iteration (reverse order of numbers)
-    a = a / 2;
+    var result = string.Empty;
+    while (input > 0)
+    {
+        result = input % 2 + result;
+        input = input / 2;
+    }
+    return result;
 }
-Console.WriteLine();
-Console.Write(bin);
+
+
+Console.WriteLine("Enter decimal number: ");
+if (!int.TryParse(Console.ReadLine()!, out var decimalNumber))
+{
+    Console.WriteLine("What a pity, can't parse your entry!");
+}
+
+var result = DecimalToBinary(decimalNumber);
+
+Console.WriteLine("Representation of decimal" + decimalNumber + " in binary system is " + result);
